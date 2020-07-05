aa16a74b038215eb7cde2d88f4ce329628dabc66
hyq <hanyanqiang@sjtu.edu.cn>
2020-07-05 18:03:46 +0800

    finish ex8

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -41,15 +41,17 @@ Theta_grad = zeros(size(Theta));
 %
 
 
+predicts = X*Theta';
+J = sum(sum(((predicts - Y).^2).*R))/2;
 
+J_reg = (sum(sum(Theta.^2))+sum(sum(X.^2)))*lambda/2;
+J = J + J_reg;
 
+X_grad = ((X*Theta').*R - Y)*Theta;
+Theta_grad = ((X*Theta'.*R) - Y)'*X;
 
-
-
-
-
-
-
+X_grad = X_grad + X*lambda;
+Theta_grad = Theta_grad + Theta*lambda;
 
 
 
--- ex8/estimateGaussian.m
+++ ex8/estimateGaussian.m
@@ -21,8 +21,9 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
+mu = sum(X)'/m;
 
-
+sigma2 = sum(bsxfun(@minus,X,mu').^2)'/m;
 
 
 
